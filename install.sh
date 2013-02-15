d3d4fb1f44b0c6de35be8344fb20b178e0651b78
Thom Leggett <thomas.leggett@hp.com>
2013-02-15 16:16:22 +0000

    Install script updates.

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-sudo apt-get update
+## Designed to deploy to a fresh Ubuntu 12.04 image.
+## Run as root or with sudo
+## You probably shouldn't run this.
 
-sudo apt-get install -y python3.2 python-pip python-virtualenv
+apt-get update
+
+apt-get install -y python3.2 python-pip python-virtualenv
 
 virtualenv .venv
 
 source .venv/bin/activate
 
 pip install -r requirements.txt
-
-python tally.py
