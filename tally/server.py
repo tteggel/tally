563fb5f0c0c33a71a330829a6c9a9ee3c18c7377
Thom Leggett <thom@tteggel.org>
2013-02-20 01:08:57 +0000

    Refactor websockets.

--- tally/server.py
+++ tally/server.py
@@ -7,6 +7,8 @@ from pubsub import pub
 import json
 import argparse
 import os
+from functools import wraps
+import logging
 
 from tally import Tally
 import version
@@ -21,12 +23,37 @@ def key404(f):
     Make KeyErrors return 404.
     Apply to any route or action that takes a key as parameter.
     """
-    def wrapped(*a, **ka):
+    def wrapper(*a, **ka):
         try:
             return f(*a, **ka)
         except KeyError:
             return abort(404, "Key Not Found")
-    return wrapped
+    return wraps(f)(wrapper)
+
+def websocket(connect=None, message=None, error=None):
+    def decorator(f):
+        def wrapped(*a, **ka):
+            wsock = request.environ.get('wsgi.websocket')
+            if not wsock:
+                # normal route execution please
+                return f(*a, **ka)
+            else:
+                # websocket request
+                try:
+                    if connect: persist = connect(wsock, *a, **ka)
+                except WebSocketError:
+                    if(error): error(wsock, *a, **ka)
+
+                while True:
+                    try:
+                        raw = wsock.receive()
+                        if (raw and message):
+                            message(wsock, raw, *a, **ka)
+                    except WebSocketError:
+                        if error: error(wsock, *a, **ka)
+                        break
+        return wraps(f)(wrapped)
+    return decorator
 
 @app.route('/')
 @view('index')
@@ -39,38 +66,37 @@ def new_action():
     key = tally.new_key()
     return redirect('/' + key)
 
+def view_tally_route_websocket_connect(wsock, key=None):
+    def key_changed(key=None, value=None):
+        wsock.send(json.dumps({'message': 'changed',
+                               'key': key,
+                               'value': value}))
+
+    pub.subscribe(key_changed, Tally.VALUE_CHANGED_TOPIC.format(key))
+
+    # nasty hack to keep this function in scope
+    # for duration of ws connection
+    return [key_changed]
+
+def view_tally_route_websocket_message(wsock, message, key=None):
+    decoded = json.loads(message)
+    if('message' in decoded
+       and decoded['message'] == 'inc'
+       and 'key' in decoded
+       and 'inc' in decoded):
+        tally.inc(key, decoded['inc'])
+
 # base route for individual tally
 key_route = '/<key:re:[' + Tally.key_space + ']*>'
 
 @app.route(key_route)
+@websocket(connect=view_tally_route_websocket_connect,
+           message=view_tally_route_websocket_message)
 @view('tally')
 @key404
 def view_tally_route(key=None):
-
-    wsock = request.environ.get('wsgi.websocket')
-    if not wsock:
-        # normal HTTP GET
-        value = tally.get(key)
-        return {'key': key, 'value': value}
-    else:
-        # websocket request
-        def key_changed(key, value):
-            wsock.send(json.dumps({'message': 'changed',
-                                   'key': key,
-                                   'value': value}))
-        pub.subscribe(key_changed, Tally.VALUE_CHANGED_TOPIC.format(key))
-        while True:
-            try:
-                raw = wsock.receive()
-            except WebSocketError:
-                break
-            if (raw):
-                decoded = json.loads(raw)
-                if('message' in decoded
-                   and decoded['message'] == 'inc'
-                   and 'key' in decoded
-                   and 'inc' in decoded):
-                    tally.inc(key, decoded['inc'])
+    value = tally.get(key)
+    return {'key': key, 'value': value}
 
 @app.post(key_route + '/inc')
 @key404
